040c674018f35af78039b6f65d93fabc2c53491d
rbr <rakshabr2001@gmail.com>
2026-08-25 12:13:06 +0000

    Area of a square using a function

---
+++ C++/area.cpp
@@ -0,0 +1,17 @@
+//area of a square
+#include<iostream>
+using namespace std;
+
+double area(double side);
+
+int main(){
+    double side;
+    cout<<"Enter the side of a square ";
+    cin>>side;
+    cout<<"Area is "<<area(side)<<" cm^2"<<endl;
+    return 0;
+}
+
+double area(double side){
+    return side*side;
+}
