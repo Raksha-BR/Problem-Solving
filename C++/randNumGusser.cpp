7d90fe7672e98cb95e71a5aac6458523fe48b26d
rbr <rakshabr2001@gmail.com>
2026-09-04 08:51:36 +0000

    Number Gussing implementation
    Implemented `Number guessing game` using `random number generator` and
    `do-while` loop

---
+++ C++/randNumGusser.cpp
@@ -0,0 +1,34 @@
+//Number Guessing game using `random number generator`
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num, guess, tries=0;
+
+    srand(time(NULL));
+    num=(rand()%100)+1; //% has higher precedence than +
+
+    cout<<"********************** NUMBER GUESSING GAME ************************\n";
+    
+    do{
+        cout<<"Enter a guess between 1 to 100: ";
+        cin>>guess;
+        tries ++;
+
+        if(guess>num){
+            cout<<"Too high!\n";
+        }
+        else if(guess<num){
+            cout<<"Too low!\n";
+        }
+        else{
+            cout<<"CORRECT GUESS! # of tries: "<<tries<<"\n";
+        }
+
+    }while(guess != num);
+
+    cout<<"********************************************************************\n";
+
+    return 0;
+}
