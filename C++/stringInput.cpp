32efa13d58b2331186d816f22a72a6225a8e8f8a
rbr <rakshabr2001@gmail.com>
2026-09-05 11:16:59 +0000

    Taking user input as strings

---
+++ C++/stringInput.cpp
@@ -0,0 +1,9 @@
+//Accept string input
+#include<iostream>
+int main(){
+    std::string name;
+    std::cout<<"What's your name? ";
+    std::cin>>name;
+    std::cout<<"\nHi "<<name<<std::endl;
+    return 0;
+}
