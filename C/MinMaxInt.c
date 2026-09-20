4a354e245b494fef51915d63a74768250c0569a9
rbr <rakshabr2001@gmail.com>
2026-09-20 15:48:17 +0000

    Program to print the max acceptable range of int variables in gcc compiler

---
+++ C/MinMaxInt.c
@@ -0,0 +1,11 @@
+//To check the min and max range of the integer
+#include<limits.h>
+#include<stdio.h>
+int main(){
+    printf("The min of int is %d and max of int range is %d",INT_MIN, INT_MAX);
+    return 0;
+}
+/*
+output:
+The min of int is -2147483648 and max of int range is 2147483647
+*/
